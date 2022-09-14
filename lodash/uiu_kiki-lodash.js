246f4212595124026dd838d13cb30620c5e9b99f
kiki <gong.com>
2022-09-14 21:37:49 +0800

    尝试提交Lodash

--- lodash/uiu_kiki-lodash.js
+++ lodash/uiu_kiki-lodash.js
@@ -1,6 +1,19 @@
 var kiki = {
 
-    chunk: function () { },
+    chunk: function (array, size = 1) {
+        var res = [],
+            start = 0;
+        var index = 0;
+        if (array.length == 0 && size <= 0 && !Number.isNaN(size)) {
+            return []
+        }
+        while (start < array.length) {
+            var tempArr = array.slice(start, (index += size));
+            res.push(tempArr);
+            start = index;
+        }
+        return res
+    },
 
     compact: function () { },
 
